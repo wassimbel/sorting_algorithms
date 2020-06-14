fe906ce9b6f2d198cfbff3f3ced813ef922aece1
wassimbel <wassim.belhedi1@gmail.com>
2020-06-14 02:02:13 -0400

    quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+void quick_sort(int *array, size_t size, int lo, int hi)
+{
+	if (lo < hi)
+	{
+		int part = lomuto_partition(array, size, lo, hi)
+		quick_sort(array, size, low, part - 1);
+		quick_sort(array, size, part + 1, hi);
+	}
+}
+
+
+int lomuto_partition(int *array, size_t size, int lo, int hi)
+{
+	int pivot = array[hi];
+	int tmp = 0, tmp2 = 0, i = lo;
+
+	for (i = lo; i < hi; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			tmp = array[i];
+			array[i] = array[lo];
+			array[lo] = tmp;
+			lo++;
+		}
+		tmp2 = array[i];
+		array[i] = hi;
+		hi = tmp2;
+	}
+	return lo;
+}
--- sort.h
+++ sort.h
@@ -22,4 +22,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size, int lo, int hi);
+int lomuto_partition(int *array, size_t size, int lo, int hi)
+
 #endif /** _SORT_H_ **/
